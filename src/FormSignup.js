eb9ed72bf2a21eba9b20bbc01a318d6d3cb5d186
tapadar13 <monsurismyname@gmail.com>
2023-03-23 21:31:01 +0530

    form designed

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -1,7 +1,66 @@
 const FormSignup = () => {
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className="form-content-right">
+      <form className="form">
+        <h1>
+          Get started with us today! Create your account by filling out the
+          information below.
+        </h1>
+        <div className="form-inputs">
+          <label htmlFor="username" className="form-label">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className="form-input"
+            placeholder="Enter your username"
+          />
+        </div>
+        <div className="form-inputs">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="form-input"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="form-inputs">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="form-input"
+            placeholder="Enter your password"
+          />
+        </div>
+        <div className="form-inputs">
+          <label htmlFor="password2" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            name="password2"
+            id="password2"
+            className="form-input"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button type="submit" className="form-input-btn">
+          Sign up
+        </button>
+        <span className="form-input-login">
+          Already have na account? Login in <a href="#">here</a>
+        </span>
+      </form>
     </div>
   );
 };
